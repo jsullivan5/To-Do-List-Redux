47639711d923970cbc8b2874ae201a3c1c9c54ef
James Sullivan <jamesfsullivan5@gmail.com>
2017-06-22 18:22:50 -0600

    Add components and hooked up app.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import './index.css'
 
-ReactDOM.render(
-  <App />,
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../src/reducers'
+
+import App from './components/App'
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={ store } >
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
